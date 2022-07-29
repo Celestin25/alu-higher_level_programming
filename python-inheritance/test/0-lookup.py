108b68148033692767d92e5d90fe200564e3371f
Celestin25 <celestinhakorimana25@gmail.com>
2022-07-29 14:48:19 +0300

    task 0

---
+++ python-inheritance/test/0-lookup.py
@@ -0,0 +1,10 @@
+"""list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Return attributes
+    Args :
+        - obj: object to get attributes
+    """
+
+    return dir(obj)
