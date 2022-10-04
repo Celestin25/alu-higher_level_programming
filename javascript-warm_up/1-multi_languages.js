d3f4dd491097a759bfc644e3608d1611b2d8edee
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-04 15:31:17 +0300

    multi languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 //  A script that prints multiple lines
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing\n';
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
