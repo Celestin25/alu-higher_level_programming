5621c9a74600c032189d61bcb3d3e2ea7bbe1eda
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-05 13:05:03 +0300

    task that prints the first arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-// A script that prints the first argument passed to it 
+// A script that prints the first argument passed to it.
 console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
