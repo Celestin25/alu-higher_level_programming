b1c8413f58f72f33be7fdc9026ad13e0b6049958
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-05 12:16:50 +0300

    task that prints many arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// A script that prints a message depending of the number of arguments passed 
+// A script that prints a message depending of the number of arguments that passed
 if (process.argv.length === 2) {
-	console.log('No argument');
-} 
+	console.log('No argument found');
+}
 if (process.argv.length === 3) {
 	console.log('Argument found');
-} 
+}
 if (process.argv.length > 3) {
 	console.log('Arguments found');
-} 
+}
