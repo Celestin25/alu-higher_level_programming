8b8fdcffc5e4949b82c9ebddc56c845ee0119177
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-06 14:58:47 +0300

    write a function that executes x times a function

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/node
 // Write a function that executes x times a function
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) theFunction();
