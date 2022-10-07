c53140d1d4a38532f3057d1bd1e3d67fa1c59f42
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-07 10:33:21 +0300

    class inherits from another

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+//write a class that defines a square and inherits from Square of 5-square.js
+module.exports = class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let s = '';
+        for (let j = 0; j < this.width; j++) {
+          s += c;
+	}
+        console.log(s);
+      }
+    }
+  }
+};
