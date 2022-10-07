77add644cf03918f09a72113162fa2f41d3c3b87
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-07 10:36:46 +0300

    class inherits from another

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//write a class that defines a square and inherits from Square of 5-square.js
+// write a class that defines a square and inherits from Square of 5-square.js
 module.exports = class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
