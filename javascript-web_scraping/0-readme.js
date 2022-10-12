dda4fc11837bc8c126b58765e0787c9c245549ff
root <root@DESKTOP-L0GB8TB.localdomain>
2022-10-12 14:28:17 +0300

    reads file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const filename = process.argv[2];
 fs.readFile(filename, 'utf-8', function (err, data) {
 	if (err) {
 	  console.log(err);
-        } else {
+       } else {
 	  console.log(data);
 	}
 });
