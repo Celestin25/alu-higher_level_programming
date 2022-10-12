421885461dd6c427f9bea4660afe35f9145178cd
root <root@DESKTOP-L0GB8TB.localdomain>
2022-10-12 14:33:45 +0300

    reads file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,10 +2,11 @@
 // write a script that reads and prints the content of a file
 const fs = require('fs');
 const filename = process.argv[2];
+
 fs.readFile(filename, 'utf-8', function (err, data) {
 	if (err) {
 	  console.log(err);
-       } else {
+        } else {
 	  console.log(data);
 	}
 });
