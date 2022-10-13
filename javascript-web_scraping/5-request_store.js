952b3ce4c295208f67407667771d6421217c0a77
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-13 16:04:16 +0300

    task

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,11 +3,5 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
-});
+const path = process.argv[3];
+request(url).pipe(fs.createWriteStream(path));
