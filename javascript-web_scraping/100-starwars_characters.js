127f5d06e37e58347865b4cf9c37878da5f4f3ae
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2022-10-14 11:32:36 +0300

    advanced task

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,23 @@
 #!/usr/bin/node
 // prints all characters of a Star Wars movie
-const req = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-req(url, function (error, response, body) {
-  if (error) { console.log(error); }
-  JSON.parse(body).characters.forEach(function (peeps) {
-    req(peeps, function (error, response, body) {
-      if (error) { console.log(error); }
-      console.log(JSON.parse(body).name);
-    });
-  });
+const request = require('request');
+const args = process.argv;
+
+request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const charEnd = data.characters;
+    const loopTime = charEnd.length;
+    for (let i = 0; i < loopTime; i++) {
+      request(charEnd[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
 });
